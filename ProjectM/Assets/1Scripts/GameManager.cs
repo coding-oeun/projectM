9ab20a3c9bddbb488079fac59ce204bf4bc8dda9
coding-oeun <92437634+coding-oeun@users.noreply.github.com>
2021-10-13 20:30:14 +0900

    초기화

---
+++ ProjectM/Assets/1Scripts/GameManager.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public bool gameClear = false;
+    public bool gameStart;
+
+    int timer;
+    public GameObject readyText;
+    public GameObject startText;
+
+    public MusicRule musicRule; // MusicRule Script 호출
+    public NewMusicRule newMusicRule;
+    public VRManager vrManager;
+    public bool movePenalty = false; //  패널티 발생 상태 체크
+    Vector3 stopPosition;
+    Vector3 currentPosition;
+
+    float penaltyDis = 1.0f;
+    private int life; // 플레이어 기회
+    public bool checkStopPoint = false; // 멈췄을 때 플레이어 위치체크 여부
+    public bool checkPenaltyYn = false;
+
+
+    void Start()
+    {
+        // 레디고 출력관련
+        gameStart = false;
+        readyText.SetActive(false);
+        startText.SetActive(false);
+
+ // 생명 3
+        life = 3;
+    }
+
+    // 1. 게임 시작 준비 (레디고)
+    void ReadyGo()
+    {
+        if (timer <= 200)
+        {
+            timer++;
+            Debug.Log(timer);
+
+            if (timer < 200)
+            {
+                readyText.SetActive(true);
+            }
+
+            if (timer >= 200)
+            {
+                readyText.SetActive(false);
+                startText.SetActive(true);
+                StartCoroutine(this.LoadingEnd());
+                gameStart = true;
+                newMusicRule.mainMusic.Play();
+                Debug.Log("gameStart True");
+            }
+
+        }
+
+    }
+
+    IEnumerator LoadingEnd()
+    {
+        yield return new WaitForSeconds(1.0f);
+        startText.SetActive(false);
+    }
+
+    // 1. VR 기기 움직임 체크
+    // 2. 음악상태 체크
+
+    bool CheckVRDevices()
+    {
+        // 음악이 정지
+        if (musicRule.startWait == true)
+        {
+            Debug.Log("움직임 체크 시작");
+            // 1. 정지 순간 위치
+            if (checkStopPoint == false)
+            {
+                stopPosition = vrManager.HmdCurrentPosCheck;
+                checkStopPoint = true;
+            }
+
+            // 2. 실시간 위치
+            currentPosition = vrManager.HmdCurrentPosCheck;
+
+            // 3. 두 위치간 거리
+            float hmdPosDis = Vector3.Distance(currentPosition, stopPosition);
+
+            Debug.Log("stop pos : " + stopPosition);
+            Debug.Log("curr pos : " + currentPosition);
+            Debug.Log("distance : " + hmdPosDis);
+
+            Debug.Log("움직인 거리 : " + hmdPosDis);
+            if (hmdPosDis > penaltyDis)
+            {
+                return true;
+            }
+
+            Debug.Log("움직임 체크 끝");
+        }
+
+        return false;
+    }
+
+    void PenaltyProcess()
+    {
+        Debug.Log("패널티 시작");
+        if (checkPenaltyYn == false)
+        {
+            life -= 1;
+            checkPenaltyYn = true;
+            Debug.Log("라이프 1 감소");
+            Debug.Log("남은 라이프 : " + life);
+        }
+
+        if (musicRule.startWait == false)
+        {
+            checkPenaltyYn = false;
+            checkStopPoint = false;
+        }
+        Debug.Log("패널티 끝");
+    }
+
+
+    void Update()
+    {
+        if (gameStart == false)
+        {
+            ReadyGo();
+        }
+
+        // 움직이면 패널티 실행
+        if (CheckVRDevices() == true)
+        {
+            PenaltyProcess();
+        }
+    }
+}
---
+++ ProjectM/Assets/1Scripts/VRManager.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRManager : MonoBehaviour
+{
+    [Header("Movement")]
+    public float ColliderTouch; //컨트롤러로 콜라이더 터치 카운트값,  move함수 관여
+    public bool isTimeLimit; // TimeLimit 코루틴 중복 방지 변수
+    public float timeCount; // 이동속도 패널티 방지 변수
+    public GameObject player; // 플레이어를 담을 변수
+
+    [Header("Device")]
+    public GameObject hmdPos; //헤드셋 위치 변수
+    public Vector3 hmdCurrentPosCheck; //헤드셋 위치 실시간 체크용(player 위치)
+    public Vector3 hmdPosStop; //패널티 지점 헤드셋 위치 체크용
+    float hmdPosDis;
+    public GameObject rightController; // 오른쪽 컨트롤러 
+    public GameObject leftController;  // 왼쪽 컨트롤러
+    Vector3 rightCurrentPos;    // 오른쪽 컨트롤러 현재 프레임 위치 변수
+    Vector3 rightCurrentPos2;   // 오른쪽 컨트롤러 다음 프레임 위치 변수
+    Vector3 leftCurrentPos;     // 왼쪽 컨트롤러 현재 프레임 위치 변수
+    Vector3 leftCurrentPos2;    // 왼쪽 컨트롤러 다음 프레임 위치 변수
+
+    void Start()
+    {
+        player = GameObject.Find("XR Rig");
+        ColliderTouch = 0f;
+        isTimeLimit = false;
+    }
+
+    void Update()
+    {
+        MoveMent();
+
+        hmdCurrentPosCheck = hmdPos.transform.position;
+        player.transform.position = new Vector3(player.transform.position.x, 1, player.transform.position.z); // 위아래 움직임 방지
+        
+        timeCount += Time.deltaTime;
+        if (timeCount > 5 && isTimeLimit == false)
+        {
+            StartCoroutine(TimeLimit());
+        }
+    }
+
+    private void LateUpdate()
+    {
+        rightCurrentPos2 = rightController.transform.position; // 다음프레임위치
+        leftCurrentPos2 = leftController.transform.position; 
+
+    }
+
+    public Vector3 HmdCurrentPosCheck
+    {
+        get
+        {
+            return hmdCurrentPosCheck;
+        }
+    }
+
+    public Vector3 Move()
+    {
+        float maximum = 2.1f;
+        float minimum = 0.1f;
+
+        if (ColliderTouch >= 5) // 증가된 속도
+        {
+
+            
+            Vector3 softmove = hmdPos.transform.forward * Mathf.Lerp(minimum, maximum, 0.1f); // 움직일때 변경되는 이동값 (서서히 증가, 감소 효과)
+            return player.transform.position = player.transform.position + softmove;
+        }
+        else // 기본 속도
+        {
+
+            minimum = 0.1f;
+            maximum = 0.09f;
+
+            Vector3 softmove = hmdPos.transform.forward * Mathf.Lerp(minimum, maximum, 0.1f); // 기본 이동값
+            return player.transform.position = player.transform.position + softmove;
+        }
+
+    }
+    IEnumerator TimeLimit() // 5초가 지나면 ColliderTouch 감소
+    {
+        isTimeLimit = true;
+        yield return new WaitForSeconds(0.1f);
+        ColliderTouch -= 0.1f; // 
+
+        if (ColliderTouch < 0) // 0 이하 방지
+            ColliderTouch = 0;
+
+        if (timeCount > 5)
+        {
+            StartCoroutine(TimeLimit());
+        }
+        else
+        {
+            isTimeLimit = false;
+        }
+    }
+
+    void MoveMent()
+    {
+        rightCurrentPos = rightController.transform.position; // 업데이트에서 계속 초기화 하면서 지금위치와 다음프레임위치가 다를때 이동발생
+        float rightPosCompare = Vector3.SqrMagnitude(rightCurrentPos - rightCurrentPos2);
+        if (rightPosCompare > 0.001f)
+        {
+            Move();
+        }
+        leftCurrentPos = leftController.transform.position; // 업데이트에서 계속 초기화 하면서 지금위치와 다음프레임위치가 다를때 이동발생
+        float leftPosCompare = Vector3.SqrMagnitude(leftCurrentPos - leftCurrentPos2);
+        if (leftPosCompare > 0.001f)
+        {
+            Move();
+        }
+    }
+}
